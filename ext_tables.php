98d6620d2f05ed6fc3f5226f329c66ce3fb3bf93
digedag <rene@system25.de>
2020-05-10 14:58:13 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- ext_tables.php
+++ ext_tables.php
@@ -17,9 +17,9 @@ if (TYPO3_MODE == 'BE') {
     ////////////////////////////////
 //     $modName = 'web_CfcLeagueM1';
 //     tx_rnbase_util_Extensions::insertModuleFunction(
-//         $modName, 
+//         $modName,
 //         System25\T3sports\Backend\Controller\StatisticsController::class,
-//         tx_rnbase_util_Extensions::extPath($_EXTKEY).'Classes/Controller/Competition.php', 
+//         tx_rnbase_util_Extensions::extPath($_EXTKEY).'Classes/Controller/Competition.php',
 //         'LLL:EXT:t3sportstats/Resources/Private/Language/locallang_db.xml:tx_t3sportstats_module_name'
 //     );
 }
