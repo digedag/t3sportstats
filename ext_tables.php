9eb8b5a89c6c5f9e197231e27e3e624bbb732c5e
René Nitzsche <rene@system25.de>
2020-06-13 10:54:42 +0200

    #23 fix static ts, travis build and composer deps

--- ext_tables.php
+++ ext_tables.php
@@ -4,8 +4,6 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-//tx_rnbase_util_Extensions::addStaticFile($_EXTKEY, 'static/ts/', 'T3sportstats');
-tx_rnbase_util_Extensions::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Plugin/', 'T3sportstats');
 
 if (TYPO3_MODE == 'BE') {
     // Einbindung einer PageTSConfig
